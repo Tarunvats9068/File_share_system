7aceac442614906335301326b29cc4b389986be3
Tarun Sharma <82764100+Tarunvats9068@users.noreply.github.com>
2023-05-27 02:27:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,12 +15,4 @@ const PORT = process.env.PORT || 5000;
 
 const fileRouter = require("./routes/route");
 app.use("/api", fileRouter);
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-    });
-}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
